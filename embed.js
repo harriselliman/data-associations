6fc7ce4b36a435411eb94875b29e3defdc8092f4
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 11:49:09 +0000

    setup user schema

--- embed.js
+++ embed.js
@@ -1,2 +1,11 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
+
+// USER - email, name
+
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String
+});
+
+var User = mongoose.model("User", userSchema);
