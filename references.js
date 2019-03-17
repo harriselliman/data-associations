3c28dcfee7acd046c38fcc16f09e0c9e7982a0f6
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 12:48:44 +0000

    add seperate post model file

--- references.js
+++ references.js
@@ -1,15 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog_demo_2", { useNewUrlParser: true });
 
-//POST - title, content
-
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-
-var Post = mongoose.model("Post", postSchema);
-
 // USER - email, name
 
 var userSchema = new mongoose.Schema({
