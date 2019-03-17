afdf77d8651e83ba399b3dd222d097708d005115
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 12:56:49 +0000

    add post to check module exports

--- references.js
+++ references.js
@@ -6,25 +6,25 @@ var User = require("./models/user");
 
 
 
-// Post.create({
-//     title: "How to cook the best burger pt.3",
-//     content: "Go to in-n-out and get a double double"
-// }, function(err, post){
-//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             foundUser.posts.push(post);
-//             foundUser.save(function(err, data){
-//                 if(err){
-//                     console.log(err);
-//                 } else {
-//                     console.log(data);
-//                 }
-//             });
-//         }
-//     });
-// });
+Post.create({
+    title: "How to cook the best burger pt.4",
+    content: "Go to in-n-out and get a double double protein style"
+}, function(err, post){
+    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save(function(err, data){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
 
 // User.create({
 //     email: "bob@gmail.com",
