a35465cc29639e270e812361c3e75883a9e759f9
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 12:41:52 +0000

    add find user

--- references.js
+++ references.js
@@ -25,27 +25,35 @@ var userSchema = new mongoose.Schema({
 
 var User = mongoose.model("User", userSchema);
 
-Post.create({
-    title: "How to cook the best burger pt.3",
-    content: "Go to in-n-out and get a double double"
-}, function(err, post){
-    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        } else {
-            foundUser.posts.push(post);
-            foundUser.save(function(err, data){
-                if(err){
-                    console.log(err);
-                } else {
-                    console.log(data);
-                }
-            });
-        }
-    });
-});
+// Post.create({
+//     title: "How to cook the best burger pt.3",
+//     content: "Go to in-n-out and get a double double"
+// }, function(err, post){
+//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             foundUser.posts.push(post);
+//             foundUser.save(function(err, data){
+//                 if(err){
+//                     console.log(err);
+//                 } else {
+//                     console.log(data);
+//                 }
+//             });
+//         }
+//     });
+// });
 
 // User.create({
 //     email: "bob@gmail.com",
 //     name: "Bob Belcher"
 // });
+
+User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user);
+    }
+});
