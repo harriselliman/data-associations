1c1bf841a40e3bbef3d78920b24d4d2cbcabde9c
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 11:58:16 +0000

    add post to post model

--- embed.js
+++ embed.js
@@ -21,15 +21,28 @@ var postModel = mongoose.model("Post", postSchema);
 
 
 
-var newUser = new User({
-    email: "charlie@brown.edu",
-    name: "Charlie Brown"
+// var newUser = new User({
+//     email: "charlie@brown.edu",
+//     name: "Charlie Brown"
+// });
+
+// newUser.save(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
+
+var newPost = new Post({
+    title: "Reflection on Apples",
+    content: "They are delicious"
 });
 
-newUser.save(function(err, user){
+newPost.save(function(err, post){
     if(err){
         console.log(err);
     } else {
-        console.log(user);
+        console.log(post);
     }
-});
+})
